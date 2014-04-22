70cfd21c031b4b1ca7d5092e888e74f70eab45f7
reconfreak <kldelp72@gmail.com>
2014-04-21 22:35:46 -0500

    Add AndriodProducts.mk

--- full_d802.mk
+++ full_d802.mk
@@ -18,10 +18,14 @@
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
 # Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+#PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, vendor/eos/config/gsm.mk)
+
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := d802
 PRODUCT_NAME := full_d802
